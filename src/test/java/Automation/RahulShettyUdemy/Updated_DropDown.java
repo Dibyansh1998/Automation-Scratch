ecf9035b059c2c6b7a18b4deeaaa908298ddab30
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-04-20 08:25:21 +0530

    Code Added

--- src/test/java/Automation/RahulShettyUdemy/Updated_DropDown.java
+++ src/test/java/Automation/RahulShettyUdemy/Updated_DropDown.java
@@ -11,21 +11,12 @@ public class Updated_DropDown {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.spicejet.com/");
-		
-		//Select the Kanpur to Delhi from the Drop Down
-		
-				driver.findElement(By.xpath("(//div[@class='css-1dbjc4n r-14lw9ot r-11u4nky r-z2wwpe r-1phboty r-rs99b7 r-1loqt21 r-13awgt0 r-ymttw5 r-tju18j r-5njf8e r-1otgn73'])[1]")).sendKeys("KANP");
 
-
-				Thread.sleep(2000);
-//				driver.findElement(By.cssSelector("[data-testid='to-testID-destination']")).click();
-				driver.findElement(By.xpath("//input[@value='Delhi (DEL)']")).click();
-		// First Click on the Passengers Drop Down
-		driver.findElement(By.xpath("//div[@data-testid='home-page-travellers']")).click();
-
+		// Click on Passengers Drop Down
+		driver.findElement(By.cssSelector("div[data-testid='home-page-travellers']")).click();
 		// Click on Adult Plus (+) Button
 		Thread.sleep(2000);
-//		driver.findElement(By.xpath("//div[@data-testid='Adult-testID-plus-one-cta']")).click();
+		driver.findElement(By.xpath("(//div[@data-testid='Adult-testID-plus-one-cta'])[1]")).click();
 
 		/*
 		 * int i=0; while(i<5) {
@@ -33,17 +24,29 @@ public class Updated_DropDown {
 		 * )).click();//it will click the (+)icon 4 times i++; }
 		 */
 		for (int i = 0; i < 3; i++) {
-			driver.findElement(By.xpath("//div[@data-testid='Adult-testID-plus-one-cta']")).click();// it will click the (+)icon 4 times
+			driver.findElement(By.xpath("(//div[@data-testid='Adult-testID-plus-one-cta'])[1]")).click();// it will
+																											// click the
+			// (+)icon 4 times
 		}
-		System.out.println(driver
-				.findElement(By.xpath("(//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep'])[5]")).getText());
 		Thread.sleep(2000);
 		// Click on the Done button on passengers Drop Down
-		driver.findElement(By.xpath("//div[@data-testid='home-page-travellers-done-cta']")).click();
-		
-		driver.findElement(By.xpath("(//div[@class='css-1dbjc4n r-18u37iz'])[35]")).click();
+		driver.findElement(By.xpath(
+				"(//div[@class='css-1dbjc4n r-1awozwy r-19m6qjp r-z2wwpe r-1loqt21 r-18u37iz r-1777fci r-d9fdf6 r-1w50u8q r-ah5dr5 r-1otgn73'])[1]"))
+				.click();
+
+		// Select the Ahemdabad to Kanpur from the Drop Down
+
+		driver.findElement(By.cssSelector(
+				".css-1dbjc4n.r-14lw9ot.r-11u4nky.r-z2wwpe.r-1phboty.r-rs99b7.r-1loqt21.r-13awgt0.r-ymttw5.r-tju18j.r-5njf8e.r-1otgn73"))
+				.click();
+		driver.findElement(By.xpath(
+				"(//div[@class='css-1dbjc4n r-18u37iz']/div[@class='css-76zvg2 r-cqee49 r-ubezar r-1kfrs79'])[3]"))
+				.click();
 		Thread.sleep(2000);
-		
+		driver.findElement(By.xpath(
+				"(//div[@class='css-1dbjc4n r-18u37iz']/div[@class='css-76zvg2 r-cqee49 r-ubezar r-1kfrs79'])[37]"))
+				.click();
+		Thread.sleep(4000);
 		driver.close();
 
 	}
