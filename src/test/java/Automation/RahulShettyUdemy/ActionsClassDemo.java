59d5b2d6f0bb48439b90f6a2442c4ac61175998f
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-06-09 09:15:02 +0530

    Stale Element Exception Class Added

--- src/test/java/Automation/RahulShettyUdemy/ActionsClassDemo.java
+++ src/test/java/Automation/RahulShettyUdemy/ActionsClassDemo.java
@@ -13,8 +13,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ActionsClassDemo {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) 
+	{
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver= new ChromeDriver();
---
+++ src/test/java/Automation/RahulShettyUdemy/ExceptionHandling.java
@@ -0,0 +1,40 @@
+package Automation.RahulShettyUdemy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		driver.get("https://omayo.blogspot.com/");
+		WebElement ele= driver.findElement(By.cssSelector("div[class='widget-content'] textarea[id='ta1']"));
+		ele.sendKeys("Dibyansh Verma");
+		
+		Thread.sleep(3000);
+		//Click on Link
+		driver.findElement(By.xpath("//a[normalize-space()='compendiumdev']")).click();
+		
+		Thread.sleep(3000);
+		//Go to parent page
+		driver.navigate().back();
+		
+		//If we missed the below step then it will give you the Stale Element Exception
+		//To avoid the exception then re-initialized it the element
+		ele= driver.findElement(By.cssSelector("div[class='widget-content'] textarea[id='ta1']"));
+		ele.clear();
+	}
+
+}
