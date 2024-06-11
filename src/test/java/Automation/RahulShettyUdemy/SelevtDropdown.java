dc5a0664ed09b2c7442d6b101685539bd3114c60
Dibyansh1998 <dibyansh.verma25@gmail.com>
2024-06-11 18:30:27 +0530

    Code Addedm

---
+++ src/test/java/Automation/RahulShettyUdemy/SelevtDropdown.java
@@ -0,0 +1,29 @@
+package Automation.RahulShettyUdemy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SelevtDropdown {
+
+	public static void main(String[] args) {
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("https://www.sugarcrm.com/au/request-demo/");
+		
+		WebElement ele=driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")));
+		ele.click();
+	}
+
+}
